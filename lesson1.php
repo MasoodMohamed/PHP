1350a81b6a0e6c51a2377268f8cff4ad62579725
MasoodMohamed <masoodbare2002@gmail.com>
2020-02-04 11:38:12 +0300

    phpfile

---
+++ lesson1.php
@@ -0,0 +1,37 @@
+<?php
+//Introduction to php
+//PHP is a widely-used, open source scripting language
+//PHP scripts are executed on servers
+//PHP files can contain texts, HTML, CSS, Javascript, and PHP Code
+//PHP code is executed on the server, and the result is returned to the browser as plain HTML
+//PHP files have extension ".php"
+echo "Hello World";
+echo "<h1>Hello World</h1>";
+//echo: print content in browser
+//Hello World: the content to be printed on the browser
+//  ;: means end of php statement or instruction
+
+
+
+//VARIABLES IN PHP
+//a container for storing data
+$myname ="MASOOD MOHAMED <br>";
+echo "$myname";
+$tech =" GOOGLE <br>";
+$tech1 =" MICROSOFT <br>";
+$tech2 =" AMAZON <br>";
+
+
+
+echo "$tech";
+echo "$tech1";
+echo "$tech2";
+
+
+
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto atque aut blanditiis, debitis
+    dolores est id labore magnam maiores modi numquam officiis perferendis perspiciatis possimus quasi
+    sint, soluta, vero voluptate.</p>
+
